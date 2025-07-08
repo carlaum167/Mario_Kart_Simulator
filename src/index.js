a4fc6c682f5b93b362a8b90a494f300074ac2371
carlaum167 <carlosgado167@gmail.com>
2025-07-08 11:45:27 -0300

    criado laço de repetição para as rodadas

--- src/index.js
+++ src/index.js
@@ -19,12 +19,21 @@ async function rollDice(){
    return Math.floor(Math.random() * 6) + 1;
 }
 
-async function playRaceEngine(character1, character2) {}
+//motor para o jogo funcionar
+async function playRaceEngine(character1, character2) {
+
+    for(let round = 1; round <=5; round++) {
+        console.log(`🏁 Rodada ${round}`);
+
+        //sorteio de bloco
+        
+    }
+}
 
 (async function main(){
     console.log(`🏁🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`      
     );
 
    await playRaceEngine(player1, player2);
-})()
+})();
 
