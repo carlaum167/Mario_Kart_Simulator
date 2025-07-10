947346fba3e259c12030e0ff9c45f98c3c484fe1
carlaum167 <carlosgado167@gmail.com>
2025-07-10 14:05:15 -0300

    criado o log da pista de confronto

--- src/index.js
+++ src/index.js
@@ -106,6 +106,33 @@ async function playRaceEngine(character1, character2) {
     if(block === "CONFRONTO"){
         let powerResult1 = diceResult1 + character1.PODER;
         let powerResult2 = diceResult2 + character2.PODER;
+
+        console.log(`${character1.NOME} confrontou o ${character2.NOME}!`);
+
+        await logRollResult(
+            character1.NOME,
+            "poder",
+            diceResult1, 
+            character1.PODER
+        );
+        await logRollResult(
+            character2.NOME,
+            "poder",
+            diceResult2, 
+            character2.PODER
+        );
+
+        character2.PONTOS -= 
+          powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1: 0;
+
+        character1.PONTOS -=
+          powerResult2 > powerResult1 && character1.PONTOS > 0 ? 1: 0;
+
+          console.log(powerResult2 === powerResult1
+             ? "Houve um empate no confronto, nenhum ponto descontado" 
+             : " "
+          );
+       
     }
 
     //verificação de vencedor
